73d9a671d1f62f3316282ce047dd243454015eec
jiha <zhsclq5324@naver.com>
2024-02-19 23:44:54 +0900

    day6 commit

---
+++ js-lv0/문자열 뒤집기.js
@@ -0,0 +1,13 @@
+function solution(my_string) {
+  var answer = "";
+  let cnt = my_string.length - 1;
+
+  while (cnt >= 0) {
+    answer += my_string[cnt];
+    cnt = cnt - 1;
+  }
+
+  return answer;
+}
+
+// return [...my_string].reverse().join("");
